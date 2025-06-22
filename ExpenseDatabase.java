de4b2b2a964b8801ebc886f7c6dd1e6db85e4404
Gayatri-2305 <challagundlagayatri23@gmail.com>
2025-06-22 19:15:21 +0530

    Create ExpenseDatabase.java

---
+++ ExpenseDatabase.java
@@ -0,0 +1,16 @@
+@Database(entities = {Expense.class}, version = 1)
+public abstract class ExpenseDatabase extends RoomDatabase {
+    private static ExpenseDatabase instance;
+
+    public abstract ExpenseDao expenseDao();
+
+    public static synchronized ExpenseDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    ExpenseDatabase.class, "expense_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+}
